8f117cbe659954b816f977caa849e23479a1cde2
Luis-Felipe-N <luisj2felipe09@gmail.com>
2023-07-26 10:47:44 -0300

    :sparkles: add auth

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,74 +1,41 @@
 'use client'
 
-import { Button } from '@/components/ui/button'
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from '@/components/ui/form'
-import { Input } from '@/components/ui/input'
-import { useForm } from 'react-hook-form'
+import { getRedirectResult, signInWithRedirect } from 'firebase/auth'
+import { useEffect, useState } from 'react'
+import { useRouter } from 'next/navigation'
+import { auth, provider } from '@/lib/firebase'
 
-export default function Login() {
-  const form = useForm()
+export default function LogIn() {
+  const router = useRouter()
 
-  const { handleSubmit, control } = form
+  useEffect(() => {
+    getRedirectResult(auth).then(async (userCred) => {
+      if (!userCred) {
+        return
+      }
 
-  function handleLogin() {}
+      console.log(userCred)
+
+      fetch('/api/auth/login', {
+        method: 'POST',
+        headers: {
+          Authorization: `Bearer ${await userCred.user.getIdToken()}`,
+        },
+      }).then((response) => {
+        if (response.status === 200) {
+          router.push('/protected')
+        }
+      })
+    })
+  }, [])
+
+  function signIn() {
+    signInWithRedirect(auth, provider)
+  }
 
   return (
-    <Form {...form}>
-      <form
-        className="space-y-4 flex flex-col"
-        onSubmit={handleSubmit(handleLogin)}
-      >
-        <FormField
-          control={control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Usuário</FormLabel>
-              <FormControl>
-                <Input
-                  className="h-14 px-4 rounded-xl border-zinc-600 placeholder:text-zinc-400"
-                  placeholder="luisnunes"
-                  {...field}
-                />
-              </FormControl>
-              <FormDescription className="text-zinc-400 text-xs ">
-                Como deseja ser chamado.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        ></FormField>
-
-        <FormField
-          control={control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Senha</FormLabel>
-              <FormControl>
-                <Input
-                  className="h-14 px-4 rounded-xl border-zinc-600 placeholder:text-zinc-400"
-                  placeholder="****"
-                  {...field}
-                />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        ></FormField>
-
-        <Button className="rounded-xl w-100 h-14 px-4 bg-green-700 hover:bg-green-800">
-          Entrar
-        </Button>
-      </form>
-    </Form>
+    <>
+      <button onClick={() => signIn()}>Sign In</button>
+    </>
   )
 }
