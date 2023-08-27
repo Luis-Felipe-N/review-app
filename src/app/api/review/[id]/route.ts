de5333c29473799a03e5e1cdc9bea69f2d3cac79
luis.nunes <luisj2felipe09@gmail.com>
2023-08-27 19:56:42 -0300

    :pencil2: Fix types

--- src/app/api/review/[id]/route.ts
+++ src/app/api/review/[id]/route.ts
@@ -1,9 +1,7 @@
 import { prisma } from '@/lib/prisma'
 import { NextResponse } from 'next/server'
 
-export async function GET(
-  { params }: { params: { id: string } },
-) {
+export async function GET({ params }: { params: { id: string } }) {
   const id = params.id
   const review = await prisma.review.findUnique({
     where: {
